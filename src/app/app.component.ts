1d7b83d3c98936ce8ff1ac65030ee13cac01dd42
mgalvez <mgalvez@frontsteps.com>
2022-04-25 17:46:53 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'hello-world';
+ export class AppComponent {
+ 
+  readonly ROOT_URL='https://nodetest-umss.herokuapp.com/people'
+ 
+  users:any;
+ 
+  constructor(private http: HttpClient){}
+ 
+  getUsers(){
+
+    this.users = this.http.get(this.ROOT_URL)
+
+  }
+ 
 }
